e301dbc3f696784b48760b892db7adad1cb0f5a2
AndreyGopko <andy-gold@mail.ru>
2018-03-27 22:06:48 +0300

    init react to do list

---
+++ src/components/app-layout/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import 'normalize-css';
+import './styles.css';
+
+class AppLayout extends Component {
+  render() {
+    return (
+      <div className="main">
+        <header className="header">
+          <h1 className="title">Welcome to React</h1>
+        </header>
+        <div className="container">
+          Hello in React.JS
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AppLayout;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppLayout from './components/app-layout';
+
+ReactDOM.render(
+    <AppLayout />,
+    document.getElementById('root')
+);
