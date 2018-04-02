85087d35d22441604f2d1ac02c91c7d20f728824
AndreyGopko <andy-gold@mail.ru>
2018-04-02 21:37:55 +0300

    add .eslintrc

--- src/components/app-layout/index.jsx
+++ src/components/app-layout/index.jsx
@@ -4,11 +4,11 @@ import './styles.css';
 class AppLayout extends Component {
   render() {
     return (
-      <div className="main">
-        <header className="header">
-          <h1 className="title">Welcome to React</h1>
+      <div className='main'>
+        <header className='header'>
+          <h1 className='title'>Welcome to React</h1>
         </header>
-        <div className="container">
+        <div className='container'>
           Hello in React.JS
         </div>
       </div>
